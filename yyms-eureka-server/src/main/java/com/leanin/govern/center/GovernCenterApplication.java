6063ab296051c0311184c48a96918ce7e34b0648
xhj <915722189@qq.com>
2019-01-07 16:58:15 +0800

    提交

--- yyms-eureka-server/src/main/java/com/leanin/govern/center/GovernCenterApplication.java
+++ yyms-eureka-server/src/main/java/com/leanin/govern/center/GovernCenterApplication.java
@@ -12,5 +12,6 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class GovernCenterApplication {
     public static void main(String[] args) {
         SpringApplication.run(GovernCenterApplication.class,args);
+
     }
 }
