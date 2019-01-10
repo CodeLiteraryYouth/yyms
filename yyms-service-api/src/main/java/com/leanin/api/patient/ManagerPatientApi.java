04026520c63d48c7c92de858e7f7f847624ac827
xhj <915722189@qq.com>
2019-01-10 16:55:31 +0800

    测试webservice

--- yyms-service-api/src/main/java/com/leanin/api/patient/ManagerPatientApi.java
+++ yyms-service-api/src/main/java/com/leanin/api/patient/ManagerPatientApi.java
@@ -2,6 +2,8 @@ package com.leanin.api.patient;
 
 import com.leanin.domain.response.DataOutResponse;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 
 import java.util.Map;
@@ -13,6 +15,8 @@ import java.util.Map;
 public interface ManagerPatientApi {
 
     @ApiOperation("患者查询接口")
+    @ApiImplicitParams(
+            @ApiImplicitParam(name = "paramMap", value = "查询信息",required = true,dataType = "map"))
     public DataOutResponse findListByParam(Map paramMap);
 
     @ApiOperation("就诊患者查询接口")
--- yyms-service-manager-patient/src/test/java/com/leanin/manager/patient/ServiceTest.java
+++ yyms-service-manager-patient/src/test/java/com/leanin/manager/patient/ServiceTest.java
@@ -4,6 +4,8 @@ import com.alibaba.fastjson.JSON;
 import com.leanin.domain.response.DataOutResponse;
 import com.leanin.manager.patient.ManagerPatientApplication;
 import com.leanin.manager.patient.service.ManagerPatientService;
+import org.apache.cxf.endpoint.Client;
+import org.apache.cxf.jaxws.endpoint.dynamic.JaxWsDynamicClientFactory;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +14,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Date;
 import java.util.HashMap;
+import java.util.Hashtable;
 import java.util.Map;
 
 @SpringBootTest
@@ -34,5 +37,26 @@ public class ServiceTest {
 //
         DataOutResponse listByParam = managerPatientService.findListByParam(map);
         System.out.println(listByParam);
+    }
+
+    @Test
+    public void testws(){
+        // 创建动态客户端
+        JaxWsDynamicClientFactory dcf = JaxWsDynamicClientFactory.newInstance();
+        Client client = dcf.createClient("http://192.168.0.131:8082/soap/test?wsdl");
+        // 需要密码的情况需要加上用户名和密码
+        // client.getOutInterceptors().add(new ClientLoginInterceptor(USER_NAME,PASS_WORD));
+//        Map paramMap=new Hashtable();
+//        paramMap.put("currentPage",1);
+//        paramMap.put("pageSize",1);
+//        paramMap.put("inOut","1");
+        try {
+//            Object[] testWS = client.invoke("testWS", paramMap);
+//            System.out.println(testWS.toString());
+            Object[] invoke = client.invoke("testString", "wbservicews");
+            System.out.println(invoke.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
