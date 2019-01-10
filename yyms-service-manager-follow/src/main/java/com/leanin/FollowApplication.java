0860eac160b98a3da118fbc9b38c09fdfba39e5a
Administrator <1134414117@qq.com>
2019-01-10 09:16:08 +0800

    随访管理模块

--- yyms-service-manager-follow/src/main/java/com/leanin/FollowApplication.java
+++ yyms-service-manager-follow/src/main/java/com/leanin/FollowApplication.java
@@ -4,6 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.scheduling.annotation.EnableAsync;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -16,6 +17,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 @EnableAsync
 @EnableEurekaClient
+@EnableHystrix
 @MapperScan("com.leanin.mapper")
 public class FollowApplication {
 
